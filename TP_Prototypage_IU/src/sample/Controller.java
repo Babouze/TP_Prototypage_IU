1592b1eeb64695faf4f852174acef5a5976ecf47
Adrien Daudel <adrien@MacBook-Pro-de-Adrien.local>
2017-02-08 16:15:52 +0100

    before TP2

--- TP_Prototypage_IU/src/sample/Controller.java
+++ TP_Prototypage_IU/src/sample/Controller.java
@@ -1,4 +1,24 @@
 package sample;
 
-public class Controller {
+import com.sun.javaws.progress.Progress;
+import javafx.event.ActionEvent;
+import javafx.fxml.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.scene.layout.Pane;
+
+import java.io.IOException;
+
+public class Controller
+{
+    @FXML
+    private void launchProgressBar(ActionEvent event)
+    {
+        Button btn = (Button) event.getSource();
+        Scene sc = btn.getScene();
+        btn.setText("Annuler");
+        ProgressBar pb = (ProgressBar) sc.lookup("#downloadBar");
+        pb.setVisible(true);
+        pb.setProgress(pb.getProgress() + 0.1);
+    }
 }
