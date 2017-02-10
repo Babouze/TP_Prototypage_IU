9305ff24fdc8ceae83d6e5cba5fed11e91dad7ad
Thomas-Garayt <thomas.garayt@gmail.com>
2017-02-10 12:44:47 +0100

    rollback

--- TP_Prototypage_IU/src/sample/Main.java
+++ TP_Prototypage_IU/src/sample/Main.java
@@ -5,6 +5,13 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
+import javafx.scene.layout.Pane;
+import org.omg.CORBA.Object;
+
+import java.io.File;
+import java.util.ArrayList;
 
 public class Main extends Application {
 
@@ -12,7 +19,16 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Crawler");
-        primaryStage.setScene(new Scene(root));
+
+        Scene sc = new Scene(root);
+        primaryStage.setScene(sc);
+
+        // ArrayList<Object> files = getArborescence("pages");
+        // displayFiles(files);
+        // System.out.println(files);
+
+        createTreeView(sc,"pages");
+
         primaryStage.show();
     }
 
@@ -20,4 +36,60 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
+    public static void createTreeView(Scene sc, String pathGet) {
+
+        TreeItem<String> rootItem = createLine("Bibliothèque");
+        TreeView<String> tree = new TreeView<>(rootItem);
+
+        File repo = new File(pathGet);
+
+        File[] fileList = repo.listFiles();
+
+        if(fileList != null) {
+            TreeItem<String> mainLine = null;
+
+            for(int i = 0 ; i < fileList.length ; i++) {
+                String nameFile = fileList[i].toString();
+
+                mainLine = createLine(nameFile);
+
+                rootItem.getChildren().add(mainLine);
+
+                // If the file has children
+                if(getArbo(nameFile)) {
+                    // createTreeView(sc,nameFile);
+                    TreeItem<String> item = new TreeItem<>("Children here");
+                    mainLine.getChildren().add(item);
+                }
+
+            }
+        }
+
+        /*
+            TreeItem<String> subLine1 = createLine("index.html");
+            TreeItem<String> subLine2 = createLine("index2.html");
+
+            TreeItem<String> item = new TreeItem<>("Message 1");
+            subLine1.getChildren().add(item);
+        */
+
+
+
+        Pane pane = (Pane) sc.lookup("#paneTreeView");
+
+        pane.getChildren().add(tree);
+    }
+
+    public static TreeItem<String> createLine(String val) {
+        TreeItem<String> item = new TreeItem<>(val);
+        item.setExpanded(true);
+
+        return item;
+    }
+
+    private static boolean getArbo(String path) {
+        return true;
+    }
+
 }
